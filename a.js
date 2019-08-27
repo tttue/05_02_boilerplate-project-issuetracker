6591967293aaa53593c4eeba3711f709e5be7818
Trần Trọng Tuệ <tttue@yahoo.com>
2019-08-27 14:31:42 +0900

    format

--- a.js
+++
@@ -1,6 +0,0 @@
-const tool = require("./tool")
-
-var s ="5d638c8dcb4f100c70d53144";
-
-var testId = /^[0-9a-f]{24}$/
-console.log(testId.test(s));
--- server.js
+++ server.js
@@ -14,18 +14,18 @@
 	npm ln dotenv
 */
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
-const helmet    = require('helmet');
-const path      = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var expect = require('chai').expect;
+var cors = require('cors');
+const helmet = require('helmet');
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config({ path: path.resolve(process.cwd(), 'environment.txt') });
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+var apiRoutes = require('./routes/api.js');
+var fccTestingRoutes = require('./routes/fcctesting.js');
+var runner = require('./test-runner');
 
 var app = express();
 
@@ -34,7 +34,7 @@ app.use(helmet.xssFilter());
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 
 
@@ -43,15 +43,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //Sample front-end
 app.route('/:project/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/issue.html');
-  });
+	.get(function (req, res) {
+		res.sendFile(process.cwd() + '/views/issue.html');
+	});
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+	.get(function (req, res) {
+		res.sendFile(process.cwd() + '/views/index.html');
+	});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
@@ -60,27 +60,27 @@ fccTestingRoutes(app);
 apiRoutes(app);
 
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+	res.status(404)
+		.type('text')
+		.send('Not Found');
 });
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
-    setTimeout(function () {
-      try {
-        runner.run();
-      } catch(e) {
-        var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
-      }
-    }, 3500);
-  }
+	console.log("Listening on port " + process.env.PORT);
+	if (process.env.NODE_ENV === 'test') {
+		console.log('Running Tests...');
+		setTimeout(function () {
+			try {
+				runner.run();
+			} catch (e) {
+				var error = e;
+				console.log('Tests are not valid:');
+				console.log(error);
+			}
+		}, 3500);
+	}
 });
 
 module.exports = app; //for testing
